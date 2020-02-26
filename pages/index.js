d521704d5833afcd46b7b5d88e042dc46dcc01ae
Ed Courage <edcourage@live.com>
2020-02-26 14:42:52 +0000

    some folders have been added to gitignore

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,13 @@ import Link from 'next/link';
 export default function Index() {
 return (
   <div>
-    <h1> Hello World! </h1>
+    <h1> Catty </h1>
+      <div>
+
+      <h4>You Random Cat!</h4>
+
+
+      </div>
       </div>
     )
 }
